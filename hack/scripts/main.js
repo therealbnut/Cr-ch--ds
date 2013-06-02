a58468710b4d0b12fec50b8473f94e5240579cbf
therealbnut <andrew.charles.bennett@gmail.com>
2013-06-02 16:08:59 +1000

    Removed the older version

--- hack/scripts/main.js
+++
@@ -1,5 +0,0 @@
-$(function(){
-	// var a = mapbox.auto('map', 'goodjessdev.map-qavt269q');
-	// var a = mapbox.auto('map', 'goodjessdev.map-p3lf1ark');
-	var a = mapbox.auto('map', 'goodjessdev.map-p3lf1ark');
-});
